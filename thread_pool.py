c2ce9b9c46d51c2adcda10961aa7e7061fd51775
Robin Gottfried <czervenka@gmail.com>
2013-06-27 10:55:49 +0200

    updated to use queues

--- thread_pool.py
+++ thread_pool.py
@@ -1,73 +1,161 @@
-__author__ = 'czervenka'
+__author__ = 'Robin Gottfried <czervenka@github.com>'
+
 """
-from thread_pool import ThreadPool
+Simple thread pool working on Google App Engine (tested on frontend).
 
-def test(n):
-    print 'Running %d' % n'
 
-t = ThreadPool()
-for n in xrange(100):
-    t.add(test, n)
-t.run()
+Examples:
+    # prints data which fits in queue
+    # using 10 threads
+    from thread_pool import ThreadPool
+    from time import sleep
+
+    def test(data):
+        print data
+
+    pool = ThreadPool()
+    pool.run()
+    for n in xrange(100):
+        pool.add(n)
+    pool.wait()
+
+Example B:
+    # thread pool chain
+    # processes data which does not fit in queue (using iterator)
+    # each result is forwarded to second pool which prints the result
+    from thread_pool import ThreadPool
+    from time import sleep
+
+    def notify_result(message):
+        print message
+
+    def process_data(n):
+        print n
+        return (notify_result, ('Data %d' % n,), {})
+
+    def source():
+        for n in xrange(10000):
+            yield (process_data, (n,), {})
+
+    # iterates throught source iterator and fills input queue
+    pool_a = ThreadPool(source=source())
+
+    # create second pool
+    pool_b = ThreadPool()
+    # ... and set pool_b to process data from pool_a
+    pool_a >> pool_b
+
+    # start the pools
+    pool_b.run()
+    pool_a.run()
+    # wait till all the workers return back from work
+    pool_b.wait()
 """
 import logging
 from threading import Thread
+from Queue import Queue, Empty, Full
+
 from time import sleep
 from datetime import datetime
 
 class ThreadPool(object):
 
-    def __init__(self, pool_size=30, tick=0.3):
+    def __init__(self, pool_size=30, source=None, queue_capacity=100, queue_timeout=0.5):
+        """
+        pool_size: maximum number of thread workers
+        source: iterator, another ThreadPool or Queue or None to use method `add` to feed workers.
+        queue_capacity: how much tasks can be queued using ThreadPool.add before Queue.Full exception is raised
+        queue_timeout: how long each thread waits for a new job from empty queue before quits
+        """
         self._pool_size = pool_size
-        self._jobs = []
         self._pool = []
-        self._tick = tick
+        self._queue = Queue(queue_capacity)
+        self._queue_capacity = queue_capacity
+        self._queue_timeout = queue_timeout
+        self._source = None
+        self._queue_out = None
+        self.set_source(source)
 
     def add(self, callback, *args, **kwargs):
-        self._jobs.append((callback, args, kwargs))
+        self._queue.put((callback, args, kwargs), timeout=self._queue_timeout)
+        return self
+
+    def wait(self):
+        self._queue.join()
+        self.finish()
+        return self
+
+    def run(self):
+        self.start()
+        self._build_pool()
+        if self._source is not None:
+            consumer = Thread(target=self._consume)
+            self._pool.append(consumer)
+            consumer.start()
+        return self
+
+    def __rshift__(self, other):
+        return self.forward(other)
+
+    def forward(self, other):
+        self._queue_out = Queue(self._queue_capacity)
+        other.set_source(self._queue_out)
+        return self
+
+    def set_source(self, source):
+        if isinstance(source, Queue):
+            self._queue = source
+        elif isinstance(source, ThreadPool):
+            source.forward(self)
+        else:
+            self._source = source
 
     def jobs_count(self):
-        return len(self._pool) + len(self._jobs)
+        return len(self._pool) + len(self._queue.qsize())
 
     def finish(self):
         self._end_time = datetime.now()
-        logging.debug('Finished in %s.' % (self._start_time - self._end_time))
+        logging.debug('Finished in %s.' % (self._end_time - self._start_time))
 
     def start(self):
         self._start_time = datetime.now()
         logging.debug('Starting.')
 
-    def tick(self):
-        pool_size = len(self._pool)
-        jobs_count = len(self._jobs)
-        logging.debug('%s running %d / %d.' % (self.__class__.__name__, pool_size, pool_size + jobs_count))
-        sleep(self._tick)
+    def _build_pool(self):
+        self._pool = []
+        for n in xrange(self._pool_size):
+            thread = Thread(target=self._thread_run)
+            self._pool.append(thread)
+            thread.start()
 
-    def run(self):
-        self.start()
-        self._feed_pool()
-        while True:
-            self._clean_pool()
-            if not self._feed_pool():
-                break
-            self.tick()
-        self.finish()
+    def _consume(self):
+        for job in self._source:
+            if not len(job) == 3:
+                raise ValueError("Job must bee tuple (callback, arg_list, kwargs_dict).")
+            while True:
+                try:
+                    self.add(job[0], *job[1], **job[2])
+                    break
+                except Full:
+                    sleep(0.1)
+        return self
 
     def _clean_pool(self):
         self._pool = filter(lambda t: t.is_alive(), self._pool)
-        if not self._pool:
-            self.finish()
 
-    def _feed_pool(self):
-        pool_size = len(self._pool)
-        while pool_size < self._pool_size:
+    def _thread_run(self):
+        while True:
             try:
-                job = self._jobs.pop(0)
-            except IndexError:
-                return 0
-            thread = Thread(target=job[0], args=job[1], kwargs=job[2])
-            self._pool.append(thread)
-            logging.debug('Adding job')
-            thread.start()
-            pool_size += 1
-        return len(self._jobs)
+                job = self._queue.get(timeout=self._queue_timeout)
+                try:
+                    logging.debug('Running job.')
+                    result = job[0](*job[1], **job[2])
+                    if self._queue_out:
+                        self._queue_out.put(result, timeout=self._queue_timeout)
+                finally:
+                    self._queue.task_done()
+            except Empty:
+                logging.debug('No jobs, quitting.')
+                break
+
+
